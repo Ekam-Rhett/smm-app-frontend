d0f9c55cccf89033428d5a533d6ecc7a11f15992
rhett-williams <rhettjwilliams@gmail.com>
2022-02-13 23:27:13 +1000

    optimise

--- src/pages/admin/loginform.js
+++ src/pages/admin/loginform.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import ReactDOM from 'react-dom';
 
 function Loginform({ Login, error }) {
   const [details, setDetails] = useState({email: "", password: ""});
